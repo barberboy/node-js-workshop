ec5fad90a0e662933f16c84df464d7ea794f6007
Ben Barber <barberboy@gmail.com>
2014-02-27 11:44:18 -0500

    Completed first challenge, with annotations.

--- challenge1/start/app.js
+++ challenge1/start/app.js
@@ -5,7 +5,8 @@ var http = require('http'),
 http.createServer(function(request, response) {
 
     // TODO: Find the ASYNCHRONOUS, NON-BLOCKING API for reading in a file.
-    fs.CHANGE_ME('./data.csv', 'utf-8', function(err, data) {
+    // http://nodejs.org/api/fs.html#fs_fs_readfile_filename_options_callback
+    fs.readFile('./data.csv', 'utf-8', function(err, data) {
         var responseData = {};
 
         // Basic JS: Work with the data in the file, and create the response
@@ -18,15 +19,19 @@ http.createServer(function(request, response) {
         });
 
         // TODO: How do we set the content type we're sending back?
+        // http://nodejs.org/api/http.html#http_response_writehead_statuscode_reasonphrase_headers
+        // http://www.ietf.org/rfc/rfc4627.txt
         response.writeHead(200, {
-            'CHANGEME':'CHANGEME'
+            'Content-Type': 'application/json'
         });
 
         // TODO: How do we serialize responseData to a JSON string?
-        response.end(CHANGE_ME.CHANGE_ME(responseData));
-
+        // http://ecma-international.org/ecma-262/5.1/#sec-15.12.3
+        response.end(JSON.stringify(responseData));
     });
 
-}).listen(3000);
+// NOTE: Cloud9 IDE requires using the PORT and IP environment variables.
+// https://docs.c9.io/writing_nodejs_hello_world.html
+}).listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0');
 
 console.log('node server running on port 3000');
