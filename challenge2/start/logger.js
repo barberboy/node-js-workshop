b2f891173966ce388dce7ad38777a8be5e741aa9
Ben Barber <barberboy@gmail.com>
2014-02-27 17:01:22 -0500

    Minor tweaks.

--- challenge2/start/logger.js
+++ challenge2/start/logger.js
@@ -1,5 +1,6 @@
 // TODO: expose a function called "info" which prints the date and a logging string.
 exports.info = function(msg) {
+    // https://developer.mozilla.org/en-US/docs/Web/API/console
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
-    console.log(new Date(), msg);
+    console.info(new Date(), msg);
 };
